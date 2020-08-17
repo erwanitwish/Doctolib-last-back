7819ad047683fc832c5cfaa0f4c885b9b406243d
erwanitwish <quillec.erwan@gmail.com>
2020-08-17 18:37:45 +0200

    modifications in conf.js

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
